f1173a9f4f16fd0c1c55590fd5ab598a75e96327
Reema Thakur <reemathakur2114@gmail.com>
2024-05-01 17:07:17 +0530

    meals page, grid, item

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,3 +1,27 @@
-const MealsPage = () => <div>Meals</div>;
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+
+const MealsPage = () => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delecious Meals, created
+          <span className={classes.highlight}> by yoy</span>
+        </h1>
+        <p>
+          Choose your favourite recipe & cook it for yourself. It is easy & fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favourite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
+};
 
 export default MealsPage;
